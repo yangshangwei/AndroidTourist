16953c079200afebda19d27a4771d70811949526
Mr.Y <815150141@qq.com>
2015-10-19 09:23:51 +0800

    JSON客户端UI

--- androidbase/src/main/java/com/turing/base/activity/HttpActivity.java
+++ androidbase/src/main/java/com/turing/base/activity/HttpActivity.java
@@ -25,22 +25,43 @@ import org.androidannotations.annotations.ViewById;
 @Fullscreen
 @EActivity(R.layout.activity_http)
 public class HttpActivity extends Activity {
+
     @ViewById(R.id.id_btn_loadWeb)
     Button btn_loadWeb;
+
     @ViewById(R.id.id_btn_loadPic)
     Button btn_loadPic;
 
     @ViewById(R.id.id_btn_httpUrlConnectionOper)
     Button btn_HttpUrlConnection_GetPost;
+
     @ViewById(R.id.id_btn_HttpClientOper)
     Button btn_HttpUrlClient_GetPost;
+
+    @ViewById(R.id.id_btn_JsonModule)
+    Button btn_json;
+
+    @ViewById(R.id.id_btn_XmlModule)
+    Button btn_xml;
+
+    @ViewById(R.id.id_btn_multiThread)
+    Button btn_multi;
+
+    @ViewById(R.id.id_btn_httpUpLoad)
+    Button btn_httpUpload;
+
+    @ViewById(R.id.id_btn_httpClientUpload)
+    Button btn_hcUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @Click({R.id.id_btn_loadWeb, R.id.id_btn_loadPic, R.id.id_btn_httpUrlConnectionOper, R.id.id_btn_HttpClientOper})
+    @Click({R.id.id_btn_loadWeb, R.id.id_btn_loadPic,
+            R.id.id_btn_httpUrlConnectionOper, R.id.id_btn_HttpClientOper,
+            R.id.id_btn_JsonModule, R.id.id_btn_XmlModule,
+            R.id.id_btn_multiThread, R.id.id_btn_httpUpLoad, R.id.id_btn_httpClientUpload})
     public void loadByURL(View clickedView) {
         switch (clickedView.getId()) {
             case R.id.id_btn_loadWeb:
@@ -54,11 +75,22 @@ public class HttpActivity extends Activity {
             case R.id.id_btn_httpUrlConnectionOper:
                 // 向自建的服务端通过Get的方式请求 其实就是一个servlet+tomcat搭建的超级入门的login，提交显示，仅仅为了演示demo 仅此而已
                 // 不能在UI线程做访问网络的耗时操作，切记！
-                // 跳转到登陆页面，输入姓名和年龄，点击提交时，开启子线程访问网络
+                // 跳转到登陆Activity，输入姓名和年龄，点击提交时，开启子线程访问网络
                 startActivity(new Intent(this, LoginActivity_.class));
                 break;
             case R.id.id_btn_HttpClientOper:
                 startActivity(new Intent(this, LoginActivity_.class));
+                break;
+            case R.id.id_btn_JsonModule:
+                startActivity(new Intent(this,JsonActivity_.class));
+                break;
+            case R.id.id_btn_XmlModule:
+                break;
+            case R.id.id_btn_multiThread:
+                break;
+            case R.id.id_btn_httpUpLoad:
+                break;
+            case R.id.id_btn_httpClientUpload:
                 break;
             default:
                 break;
---
+++ androidbase/src/main/java/com/turing/base/activity/JsonActivity.java
@@ -0,0 +1,16 @@
+package com.turing.base.activity;
+
+import android.app.Activity;
+
+import com.turing.base.R;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+
+@Fullscreen
+@EActivity(R.layout.activity_json)
+public class JsonActivity extends Activity {
+
+
+
+}
