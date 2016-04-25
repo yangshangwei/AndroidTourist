bc0412b47a748177f87d80c9e4bc2e310f2e88c8
Mr.Y <815150141@qq.com>
2016-04-26 00:23:34 +0800

    通过自定义PopupWindow实现QQ菜单选项功能

--- androidbase/src/main/java/com/turing/nutritiousSerial/listPopupWindow/customPopupWindow/ListPopupWindow.java
+++ androidbase/src/main/java/com/turing/nutritiousSerial/listPopupWindow/customPopupWindow/ListPopupWindow.java
@@ -3,10 +3,13 @@ package com.turing.nutritiousSerial.listPopupWindow.customPopupWindow;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.turing.base.R;
 import com.turing.nutritiousSerial.listPopupWindow.adapter.CustomPopupWindowAdpater;
@@ -29,8 +32,7 @@ public class ListPopupWindow extends PopupWindow {
     private View parentView;
     //item数据源
     private List<PopupItemBean> dataList;
-    //item列表视图
-    private ListView lv;
+
     //适配器
     private CustomPopupWindowAdpater adapter;
 
@@ -56,12 +58,18 @@ public class ListPopupWindow extends PopupWindow {
     /**
      * 构造函数
      */
-    public ListPopupWindow(Context context, List<PopupItemBean> dataList,
-                           View parentView) {
+    public ListPopupWindow(Context context,
+                           List<PopupItemBean> dataList,
+                           View parentView,
+                           OnPopubItemClickListener popupItemListener,
+                           OnBottomTextViewClickListener bottomTextViewListener) {
+
         this.context = context;
         this.dataList = dataList;
         this.parentView = parentView;
 
+        this.popupItemListener = popupItemListener;
+        this.bottomTextViewListener = bottomTextViewListener;
 
         initCustomPopupWindow();
     }
@@ -71,28 +79,66 @@ public class ListPopupWindow extends PopupWindow {
      * 初始化自定义的PopupWindow
      */
     private void initCustomPopupWindow() {
-
-        parentView = LayoutInflater.from(context).inflate(R.layout.list_popupwindow,null);
+        // 加载自定义布局文件，转化为组件
+        parentView = LayoutInflater.from(context).inflate(R.layout.list_popupwindow, null);
+        // 设置显示的view
         setContentView(parentView);
 
+        // 初始化控件
+        ListView lv = (ListView) parentView.findViewById(R.id.id_lv_popupWindowListView);
+        TextView tv = (TextView) parentView.findViewById(R.id.id_tv_bottom);
 
-        lv = (ListView) parentView.findViewById(R.id.id_lv_popupWindowListView);
-
+        // 设置弹出窗体的高
         this.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
         this.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
-
+        // 设置弹出窗体可点击
         this.setFocusable(true);
+        // 设置SelectPicPopupWindow弹出窗体的背景
         this.setBackgroundDrawable(new ColorDrawable(0xb0000000));
 
+
+        // view添加OnTouchListener监听判断获取触屏位置如果在布局外面则销毁弹出框
+        parentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // getTop View自身的顶边到其父布局顶边的距离,因为是根目录所以为0
+                int height = parentView.findViewById(R.id.id_rl_relativeLayout).getTop();
+                // getY 点击事件距离控件顶边的举例
+                int y = (int) event.getY();
+                // 当抬起 并且 y>height时（也就是 只要不点击ListView范围内）,dismiss popupWindow
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (y > height) {
+                        dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+        // 更新位置和大小(不加这行代码也行)
         update();
-
-
-        adapter = new CustomPopupWindowAdpater(context,dataList);
+        // 实例化适配器
+        adapter = new CustomPopupWindowAdpater(context, dataList);
+        // 设置适配器
         lv.setAdapter(adapter);
 
 
+        // ListView设置点击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 回调onPopupItemClick
+                popupItemListener.onPopupItemClick(view, position);
+            }
+        });
 
-
+        // TextView设置点击事件
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 回调onBottomClick
+                bottomTextViewListener.onBottomClick();
+            }
+        });
 
     }
 }
--- androidbase/src/main/java/com/turing/nutritiousSerial/listPopupWindow/test/ListPopupWindowDemoActivity.java
+++ androidbase/src/main/java/com/turing/nutritiousSerial/listPopupWindow/test/ListPopupWindowDemoActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.turing.base.R;
+import com.turing.base.utils.AlertUtil;
 import com.turing.nutritiousSerial.listPopupWindow.beans.PopupItemBean;
 import com.turing.nutritiousSerial.listPopupWindow.customPopupWindow.ListPopupWindow;
 
@@ -17,6 +18,10 @@ public class ListPopupWindowDemoActivity extends AppCompatActivity implements Li
 
     // 定义父View即PopupWindow依赖浮动的View
     private RelativeLayout relativeLayout;
+
+
+    private ListPopupWindow popWindow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +51,8 @@ public class ListPopupWindowDemoActivity extends AppCompatActivity implements Li
         datalists.add(itemBean3);
 
         // 实例化自定义ListPopupWindow
-        ListPopupWindow popWindow = new ListPopupWindow(ListPopupWindowDemoActivity.this,
-                datalists, relativeLayout);
+        popWindow = new ListPopupWindow(ListPopupWindowDemoActivity.this,
+                datalists, relativeLayout, this, this);
         // 居中并且靠底部显示
         popWindow.showAtLocation(relativeLayout, Gravity.CENTER | Gravity.BOTTOM, 0, 0);
     }
@@ -55,11 +60,25 @@ public class ListPopupWindowDemoActivity extends AppCompatActivity implements Li
 
     @Override
     public void onBottomClick() {
+        popWindow.dismiss();
+        AlertUtil.showToastShort(ListPopupWindowDemoActivity.this, "点击取消");
 
     }
 
     @Override
     public void onPopupItemClick(View view, int postion) {
-
+        switch (postion) {
+            case 0:
+                AlertUtil.showToastShort(ListPopupWindowDemoActivity.this, String.valueOf(postion) + " 被点击");
+                break;
+            case 1:
+                AlertUtil.showToastShort(ListPopupWindowDemoActivity.this, String.valueOf(postion) + " 被点击");
+                break;
+            case 2:
+                AlertUtil.showToastShort(ListPopupWindowDemoActivity.this, String.valueOf(postion) + " 被点击");
+                break;
+            default:
+                break;
+        }
     }
 }
